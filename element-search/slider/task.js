81b71d331bc699819b2a4142b78a7010230e05e8
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-09 08:46:51 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,34 +1,47 @@
-(() => {
-    let currentSlide = 0;
-    const sliderPrev = document.body.querySelector(".slider__arrow_prev");
-    sliderPrev.onclick = function () {
-        const menu = document.body.querySelectorAll(".slider__item");
-        if (currentSlide == 0) {
-            menu[currentSlide].classList.remove('slider__item_active')
-            currentSlide = 4;
-            menu[currentSlide].classList.add('slider__item_active')
-        } else {
-            currentSlide -= 1;
-            menu[currentSlide].classList.add('slider__item_active')
-            let current = currentSlide + 1;
-            menu[current].classList.remove('slider__item_active');
-        }
-    }
-    const sliderNext = document.body.querySelector(".slider__arrow_next");
-    sliderNext.onclick = function () {
-        const menu = document.body.querySelectorAll(".slider__item");
-        if (currentSlide >= 0) {
-            menu[currentSlide].classList.remove('slider__item_active')
-            if (currentSlide == menu.length - 1) {
-                menu[4].classList.remove('slider__item_active')
-                currentSlide = 0;
-                menu[currentSlide].classList.add('slider__item_active')
-            } else {
-                currentSlide += 1;
-                menu[currentSlide].classList.add('slider__item_active')
-                let current = currentSlide - 1;
-                menu[current].classList.remove('slider__item_active')
-            }
-        }
-    };
-})();
+var slideNumber = 0;
+	var sliderItems = document.getElementsByClassName("slider__item");
+	var sliderDots = document.getElementsByClassName("slider__dot");
+	
+	function prevSlide() {
+		slideNumber = (slideNumber == 0) ? sliderItems.length - 1 : slideNumber - 1;
+	}
+	
+	function nextSlide() {
+		slideNumber = (slideNumber == sliderItems.length - 1) ? 0 : slideNumber + 1;
+	}
+	
+	function customSlide(targetDotElement) {
+		slideNumber = 0;
+		var dotElem = targetDotElement.previousElementSibling;
+		while (dotElem) {
+			slideNumber++;
+			dotElem = dotElem.previousElementSibling;
+		}
+	}
+	
+	function changeSlide(func, targetDotElement) {
+		sliderItems[slideNumber].classList.remove("slider__item_active");
+		sliderDots[slideNumber].classList.remove("slider__dot_active");
+		func(targetDotElement);
+		sliderItems[slideNumber].classList.add("slider__item_active");
+		sliderDots[slideNumber].classList.add("slider__dot_active");
+	}
+	
+	const setOnClickEventForClass = (elementClassName, func) => {
+		Array.from(document.getElementsByClassName(elementClassName)).forEach(element => {
+			element.onclick = func;
+		})
+	}
+	
+	setOnClickEventForClass("slider__arrow_prev", () => {
+		changeSlide(prevSlide)
+	});
+	setOnClickEventForClass("slider__arrow_next", () => {
+		changeSlide(nextSlide)
+	});
+	setOnClickEventForClass("slider__dot", (e) => {
+		changeSlide(customSlide, e.target);
+	});
+	
+	changeSlide(customSlide, sliderDots[slideNumber]);
+
