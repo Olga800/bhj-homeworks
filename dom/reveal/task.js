c62ea9b2d17dba08cf62b57c5ceef8ecd93b1ccc
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-15 16:44:47 +0300

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -5,6 +5,6 @@ window.addEventListener('scroll', event => {
     const massageTopX = message.getBoundingClientRect().top;
     const massageBottomX = message.getBoundingClientRect().bottom;
     const viewportHeight = window.innerHeight;
-    const condition = (massageTopX < (viewportHeight / 1.5) & massageBottomX >= (viewportHeight / 4))
+    const condition = (massageTopX < (viewportHeight / 1.5) & massageBottomX >= (viewportHeight / 4));
     condition ? message.classList.add('reveal_active') : message.classList.remove('reveal_active');
 });
