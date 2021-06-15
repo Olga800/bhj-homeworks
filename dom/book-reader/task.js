e17e8df3686b3fa903432683398db149f925c323
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-15 16:43:52 +0300

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -9,18 +9,18 @@ for (let i = 0; i < fontSize.length; i++) {
       fontSize[s].classList.remove("font-size_active");
       if (bookReader.classList.contains("book_fs-big")) {
         bookReader.classList.remove("book_fs-big");
-      }
+      };
       if (bookReader.classList.contains("book_fs-small")) {
         bookReader.classList.remove("book_fs-small");
-      }
-    }
+      };
+    };
 
     fontSize[i].classList.add("font-size_active");
     if (fontSize.item(0).classList.contains("font-size_active")) {
       bookReader.classList.add("book_fs-small");
-    }
+    };
     if (fontSize.item(2).classList.contains("font-size_active")) {
       bookReader.classList.add("book_fs-big");
-    }
+    };
   });
 };
