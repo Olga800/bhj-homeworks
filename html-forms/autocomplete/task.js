ae556e7ad6a359a9d62b4d225c6a3d5ffbfe7efb
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-21 09:10:31 +0300

    Update task.js

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -8,8 +8,7 @@ class Autocomplete {
     this.valueElement = container.querySelector( '.autocomplete__text-content' );
 
     this.registerEvents();
-  }
-
+  };
   registerEvents() {
     this.valueContainer.addEventListener( 'click', e => {
       this.searchInput.classList.add( 'autocomplete__search_active' );
@@ -19,18 +18,15 @@ class Autocomplete {
 
       this.onSearch();
     });
-
-
     this.searchInput.addEventListener( 'input', e => this.onSearch());
 
     this.list.addEventListener( 'click', e => {
       const { target } = e;
       if ( !target.matches( '.autocomplete__item' )) {
         return;
-      }
-
-      const { textContent: text } = target,
-        { id: value, index } = target.dataset;
+      };
+      const { textContent: text } = target, 
+            { id: value, index } = target.dataset;
 
       this.onSelect({
         index,
@@ -38,22 +34,18 @@ class Autocomplete {
         value
       });
     });
-  }
-
+  };
   onSelect( item ) {
     this.input.selectedIndex = item.index;
     this.valueElement.textContent = item.text;
-
     this.searchInput.classList.remove( 'autocomplete__search_active' );
     this.list.classList.remove( 'autocomplete__list_active' );
-  }
-
+  };
   onSearch() {
     const matches = this.getMatches( this.searchInput.value );
 
     this.renderMatches( matches );
-  }
-
+  };
   renderMatches( matches ) {
     const html = matches.map( item => `
     	<li>
@@ -65,29 +57,20 @@ class Autocomplete {
     `);
 
     this.list.innerHTML = html.join('');
-  }
+  };
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    let selectFiltered = [];
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
-  }
-}
+    for (let item of this.input) {
+      if ( item.text.includes(text) ) {
+        selectFiltered.push({
+          text: `${item.text}`,
+          value: `${item.value}`;
+        });
+      };
+    };
 
-new Autocomplete( document.querySelector( '.autocomplete' ));
+    return selectFiltered;
+  };
+};
