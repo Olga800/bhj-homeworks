df0fadd4811bcc43e890f5d4ac731799afe014d5
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-24 16:12:40 +0300

    Update task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,28 +1,74 @@
-`use strict`;
+'use strict';
 
-const interestGroup = document.querySelectorAll(`input.interest__check`);
+const checkBoxesArray = Array.from(document.querySelectorAll('input.interest__check'))
 
-for (let group of interestGroup) {
-    
-    group.addEventListener(`click`, e => {
-
-        if ( !group.closest(`.interests_active`) && group.checked === true) {
-            const currentParent = group.closest(`.interest`);
-            const currentInterestGroup = currentParent.querySelectorAll(`.interest__check`);
-
-            for (let item of currentInterestGroup) {
-                item.checked = true;                
+// flag checked for children
+function setChildrenChecked(node, tagName, isChecked) {
+    try {
+        node.childNodes.forEach(function (item) {
+            if (item.nodeName == tagName.toUpperCase()) {
+                item.indeterminate = false;
+                isChecked ? item.checked = true : item.checked = false;
             };
+            setChildrenChecked(item, tagName, isChecked);
+        });
+    } catch {};
+};
 
-        }; else if ( !group.closest(`.interests_active`) && group.checked === false) {
-            const currentParent = group.closest(`.interest`);
-            const currentInterestGroup = currentParent.querySelectorAll(`.interest__check`);
-
-            for (let item of currentInterestGroup) {
-                item.checked = false;                
+// flag indeterminate for parents
+function setParentsChecked(node, tagName, isNeighbourChecked, isChecked) {
+    try {
+        let condition = false;
+        node = node.closest('ul.interests');
+        while (!condition) {
+            condition = ((node.parentElement.tagName == 'UL') && (node.parentElement.className == ''));
+            let tagInput = node.closest('li.interest').querySelector(tagName);
+            tagInput.indeterminate = true;
+            if (isChecked) {
+                tagInput.indeterminate = true;
+            } else {
+                if (isNeighbourChecked) {
+                    tagInput.indeterminate = true;
+                } else {
+                    tagInput.indeterminate = false;
+                    tagInput.checked = false;
+                };
             };
-
+            node = node.parentNode;
         };
-        
+    } catch {};
+};
+
+// fill parents base in children
+function setParentFull(array) {
+    array.forEach(function (item) {
+        let checkBoxList = Array.from(item.closest('li').querySelectorAll('input'));
+        for (let checkBox in checkBoxList) {
+            if (!(checkBoxList[checkBox].checked ? true : checkBoxList[checkBox].indeterminate ? true : false)) {
+                return false;
+            };
+        };
+        item.indeterminate = false;
+        item.checked = true;
     });
 };
+
+// check neighbours status
+function isNeighbourChecked(neighbourArray) {
+    for (let neighbour in neighbourArray) {
+        if (neighbourArray[neighbour].checked) {
+            return true;
+        };
+    };
+    return false;
+};
+
+// event function
+checkBoxesArray.forEach(function (item) {
+    item.addEventListener('change', function (event) {
+        let node = item.closest('li');
+        setChildrenChecked(node, 'input', item.checked);
+        setParentsChecked(node, 'input', isNeighbourChecked(item.closest('ul').querySelectorAll('input')), item.checked);
+        setParentFull(checkBoxesArray);
+    });
+});
