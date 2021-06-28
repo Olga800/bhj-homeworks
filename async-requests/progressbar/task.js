f5c39ea0a54f789790227b5fb970b6478fbfc90c
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-28 13:52:48 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,18 +1,16 @@
-let form = document.getElementById("form");
-let progress = document.getElementById("progress");
+'use strict';
 
-form.addEventListener('submit', () => {
-    var formData = new FormData(form);
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    xhr.send(formData);
-
-    xhr.upload.onprogress = function() {
-        alert( 'Загружено на сервер');
-    }
-
-    xhr.upload.onprogress = function(event) {
-        console.log(event);
-    }
-})
+document.getElementsByTagName('input').file.onchange = function(event) {
+    let file = this.files[0];
+    document.getElementById('send').onclick = function() {
+        const request = new XMLHttpRequest
+        request.upload.onprogress = function (event) {
+            const progress = document.getElementById( 'progress' );
+            progress.value = (event.loaded / event.total).toFixed(3);
+            // console.log('ProgressBar: ' + progress.value);
+        };
+        request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+        request.send(file);
+        return false;
+    };
+};
